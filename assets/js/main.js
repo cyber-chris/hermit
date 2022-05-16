03cdf499f5b7d3c4a7bd9408a2d93ecdfbb4b210
cyber-chris <me@cyberchris.xyz>
2022-05-16 14:02:00 +0100

    try a lighter colour

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,7 +57,7 @@ const drawGrid = (ctx) => {
     	ctx.strokeStyle = 'grey';
       ctx.strokeRect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
       if (grid[y][x]) {
-        ctx.fillStyle = 'purple';
+        ctx.fillStyle = 'deepskyblue';
         ctx.fillRect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
       }
     }
